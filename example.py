75480146976758303c5243cfe7922564b277bf5b
Molodec2020 <88512547+Molodec2020@users.noreply.github.com>
2023-10-31 23:38:00 +0300

    Veersion - 1.0.2 Add func CREATE TABLE and update func printmn Add if - elif

--- example.py
+++ example.py
@@ -2,6 +2,7 @@ import sqlite3 as sq
 
 
 def func_decorator(func):
+    # декоратор, который подключается к базе и выполняет функцию func.
     def connect_bd(*args, **kwargs):
         with sq.connect('jianShe.db') as con:
             cur = con.cursor()
@@ -21,11 +22,22 @@ def print_date():
     return 'SELECT date FROM jianSheTable'
 
 def printmn():
+    # Печатает примерную зп за весь месяц
     a = print_money()
     my_sum = 0
     for i in a:
         my_sum += float(i)
     print(round(my_sum - (my_sum * 13 / 100), 2))
-    print('До вычета налога:', my_sum)
+    print('До вычета налога:', round(my_sum, 2))
 
-printmn()
+@func_decorator
+def create_table():
+    return input('Введи SQL команду: ')
+
+
+val = input('Узнать зп за месяц - зп, SQL запрос - sql: ')
+
+if val[0].lower() == 'з':
+    printmn()
+elif val[0].lower() == 's':
+    create_table()
